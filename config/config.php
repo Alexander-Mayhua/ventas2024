98e45c56eef0df07b1a424aad40e57d405edd7c9
Alexander-Mayhua <alexander.mayhua12@gmail.com>
2024-10-10 09:17:40 -0500

    Delete config.php

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-const BD_HOST ='localhost';
-const BD_NAME = 'sistema_ventas';
-const BD_USER = 'root';
-const BD_PASSWORD = '';
-//ingresar  link del proyecto//
- const  BASE_URL ='http://localhost/ventas2024/';
-?>
