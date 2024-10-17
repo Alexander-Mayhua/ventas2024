263eef691786c051f7266081bc4e52ed9b4be225
Alexander-Mayhua <alexander.mayhua12@gmail.com>
2024-10-17 16:14:51 +0200

    Create nuevo-producto.php

---
+++ views/nuevo-producto.php
@@ -0,0 +1,26 @@
+<p>FORMULARIO DE REGISTRO DE PRODUCTOS</p>
+<form action="">
+    <label for="codigo">codigo</label>
+    <input type="text"  name="codigo" placeholder="codigo">
+
+    <label for="nombre">nombre</label>
+    <input type="text"  name="nombre" placeholder="nombre">
+
+    <label for="detalle">Detalle</label>
+    <input type="text"  name="detalle" placeholder="detalle">
+
+    <label for="precio">Precio</label>
+    <input type="text"  name="precio" placeholder="precio">
+
+    <label for="stok-inicial">stok Inicial</label>
+    <input type="text"  name="stok-inicial" placeholder="stok inicial">
+
+    <label for="categoria">Categoria</label>
+    <input type="text"  name="categoria" placeholder="categoria">
+
+    <label for="imagen">imagen</label>
+    <input type="text"  name="imagen" placeholder="imagen">
+
+    <label for="proveedor">Proveedor</label>
+    <input type="text"  name="proveedor" placeholder="proveedor">
+</form>
