3efea8afa0bca51f274cd2886f17d7d4a7522b97
Alexander-Mayhua <alexander.mayhua12@gmail.com>
2024-11-14 17:20:36 -0500

    s

--- model/personaModel.php
+++ model/personaModel.php
@@ -20,9 +20,9 @@ class personaModel{
         $sql= $this->conexion->query("UPDATE producto set imagen='{$imagen}' where id='{$id}'");
         return 1;
     }*/
-    public function buscarPersonaPorDni($nro_identidad){
+    public function buscarPersonaPorDni($Dni){
         $sql= $this->conexion->query("SELECT * FROM persona where
-      nro_identidad='{$nro_identidad}'");
+      nro_identidad='{$Dni}'");
       $sql =$sql->fetch_object();
       return $sql;
 
