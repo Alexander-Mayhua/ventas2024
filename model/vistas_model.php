47a6cd81981e03222e7f34d0afc99e6ce0b81ff8
Alexander-Mayhua <alexander.mayhua12@gmail.com>
2024-11-28 14:17:29 +0100

    log

--- model/vistas_model.php
+++ model/vistas_model.php
@@ -5,10 +5,10 @@ protected static function obtener_vistas($vista){
     $palabras_permitidas = ['inicio','carrito','varones','mujeres','detalles', 'categorias', 'persona','compras',
 
     'perfil','informacion','nuevo-producto','nuevo-categoria','nueva-compra','niños','niñas','usuario','login','nuevo-usuario','productos','nuevo-persona'];
-   /*if(!isset($_SESSION['sesion_ventas_id'])){ //si no existe la variable sesion returna a login
+   if(!isset($_SESSION['sesion_ventas_id'])){ //si no existe la variable sesion returna a login
 
   return "login";
-   }*/
+   }
     if (in_array($vista,$palabras_permitidas)){
         if(is_file("./views/".$vista.".php")){
             $contenido = "./views/".$vista.".php";
